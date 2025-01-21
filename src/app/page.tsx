9a00fcaaff3b45a12bd9742f0bfefd664ec32028
devilal0214 <devilal0214@gmail.com>
2025-01-21 19:24:23 +0530

    basic setup, shadcn ui, theme-provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
+import { Button } from "@/components/ui/button";
+import ThemeToggle from "@/components/ui/shared/theme-toggle";
 
 
 export default function Home() {
   return (
-    <div>
-      <h1>
+    <div className="p-5">
+      <div  className="w-100 justify-end flex ">
+
+      <ThemeToggle></ThemeToggle>
+
+      </div>
+      <h1 className="font-light text-blue-600 font-roboto">
         Welcome to the commerce platform
       </h1>
+      <Button variant={"destructive"}>Click here</Button>
     </div>
   );
 }
---
+++ src/components/ui/shared/theme-provider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { ThemeProvider as NextThemesProvider} from "next-themes";
+import { useEffect, useState } from "react";
+
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  // Prevent rendering until the theme is loaded
+  if (!mounted) return <div className="invisible" />;
+
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="light"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </NextThemesProvider>
+  );
+}
